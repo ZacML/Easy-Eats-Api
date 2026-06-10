7cc316164871c07b661974dc332bd8bf3020148f
João Vitor <joaovitorbatista0104@gmail.com>
2026-06-09 21:35:54 -0300

    Add files via upload

---
+++ src/main/java/com/easy/eats/EatsApplication.java
@@ -0,0 +1,13 @@
+package com.easy.eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy/eats/EatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
